867ee30c055e30aaf22c8ac59ee77d1d6f57a365
HedgeEdge <21je0571@iitism.ac.in>
2024-10-27 15:40:18 +0530

    navbar update..

--- client/src/Container.js
+++ client/src/Container.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/Header';
 import Recipes from './components/Recipes';
 import Products from './components/Products';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function Container() {
   return (
     <div className="Container">
+      <Navbar />
       <Header />
       <Recipes />
       <Products />
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import { FaBars, FaTimes, FaSearch } from 'react-icons/fa';
+import logo_tca from '../assets/logo.jpeg';
+
+function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
+  return (
+    <header>
+      <img src={logo_tca} alt="Logo" className='logo' />
+      
+      {/* Search Field for Desktop and Tablet */}
+      <span className='desktop-search-container'>
+        <input 
+          type="text" 
+          className="desktop-search-input" 
+          placeholder="Search for recipes, products..."
+        />
+      </span>
+<nav className={isMenuOpen ? 'responsive_nav' : ''}>
+        <a href='/#'>Home</a>
+        <a href='/#'>Recipe</a>
+        <a href='/#'>Products</a>
+        <a href='/#'>Amazon</a>
+        <a href='/#'>Work</a>
+        <a href='/#'>About Us</a>
+        <button className='shop'>Shop</button>
+
+        <button className='nav-btn close' onClick={toggleNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+
+      {/* Mobile Search Field Toggle */}
+      <button className='search-icon' onClick={toggleSearch}>
+        <FaSearch />
+      </button>
+
+      {/* Search Field for Mobile */}
+      {isSearchOpen && (
+        <div className="mobile-search-container">
+          <input 
+            type="text" 
+            className="mobile-search-input" 
+            placeholder="Search for recipes, products, or anything..." 
+          />
+        </div>
+      )}
+
+      {/* Hamburger Icon */}
+      <button className='nav-btn' onClick={toggleNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+}
+
+export default Navbar;
